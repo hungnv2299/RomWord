f478b2cbcf95dc6e4c5c9dd35da6b31526d24540
Hung2299 <hungkoj9x@gmail.com>
2021-07-13 14:03:19 +0700

    first commit

---
+++ app/src/main/java/com/example/android/roomwordssample/WordRepository.kt
@@ -0,0 +1,19 @@
+package com.example.android.roomwordssample
+
+import androidx.annotation.WorkerThread
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Abstracted Repository as promoted by the Architecture Guide.
+ * https://developer.android.com/topic/libraries/architecture/guide.html
+ */
+class WordRepository(private val wordDao: WordDao) {
+
+    val allWords: Flow<List<Word>> = wordDao.getAlphabetizedWords()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
